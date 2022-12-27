ac270e73601c6d88c04fab0d23268e0d75f7470e
SreenathSreekrishna <sreenathsreekrishna21@gmail.com>
2022-12-27 18:06:04 +0530

    finish 10th problem (could have used optimized approach but whatever)

---
+++ c/p10.c
@@ -0,0 +1,23 @@
+//https://projecteuler.net/problem=10
+//Summation of primes
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    long sum = 0;
+    int n = 2000000;
+    for (int i = 2; i<n; i++) {
+        int isPrime = 1;
+        for (int j = 2; j<=sqrt(i); j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+        if (isPrime) {
+            sum += i;
+        }
+    }
+    printf("%li\n", sum);
+}
