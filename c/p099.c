54631721ae5bbf0085ce99073b32d5bac34d880c
SreenathSreekrishna <sreenathsreekrishna21@gmail.com>
2024-12-15 17:31:48 +0530

    idk if using builtin log is cheating, but i did it ayways to solve p099

---
+++ c/p099.c
@@ -0,0 +1,35 @@
+//https://projecteuler.net/problem=99
+//Largest Exponential
+
+#include <stdio.h>
+#include <math.h>
+
+int cmp(int a, int p1, int b, int p2) {
+    //returns if a^p1 > b^p2
+    double v1 = (double) p1*(double) log(a);
+    double v2 = (double) p2 * (double) log(b);
+    printf("%f %f\n", v1,v2);
+    return  v1> v2;
+}
+
+int main(void) {
+    FILE *f = fopen("../files/p099_base_exp.txt", "r");
+    if (f == NULL) {
+        printf("Error opening input file!\n");
+        return 1;
+    }
+
+    int max = 2;
+    int maxp = 2;
+    int maxi;
+    for (int i = 0; i<1000; i++) {
+        int n,p;
+        fscanf(f,"%d,%d\n",&n,&p);
+        if (cmp(n,p,max,maxp)) {
+            max = n;
+            maxp = p;
+            maxi = i;
+        }
+    }
+    printf("%d\n", maxi+1);
+}
