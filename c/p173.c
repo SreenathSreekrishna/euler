1db1717d04dbb4eac7d1d3a11ce49930801ec0ba
SreenathSreekrishna <sreenathsreekrishna21@gmail.com>
2025-10-27 01:04:45 -0400

    stupid me forgor to add this crucial step to optimize

--- c/p173.c
+++ c/p173.c
@@ -8,7 +8,7 @@ int main(void) {
     //for eg. 32 = 6^2 - 2^2 = 9^2 - 7^2 = 2*16 = 4*8
     //Thus, the number of ways for n is just number of factors < sqrt(n) such that f,n/f both even
     int ans = 0;
-    for (int i = 4; i<=1000000; i++) {
+    for (int i = 4; i<=1000000; i+=4) {
         for (int f = 2; f*f < i; f+=2) {
             if (i%f) continue;
             if (i/f % 2) continue;
