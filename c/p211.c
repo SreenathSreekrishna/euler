59ea5a3b5411b3b5a7c2c1fb4443860ede9d697b
SreenathSreekrishna <sreenathsreekrishna21@gmail.com>
2025-11-26 11:48:35 -0500

    solved p211 pretty easily with a divisor sieve method that i used in a previous problem (i forgot which one) for counting number of divisors for all numbers upto N. initially tried prime factorization but it was wayy too slow

---
+++ c/p211.c
@@ -0,0 +1,34 @@
+//https://projecteuler.net/problem=211
+//Divisor Square Sum
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define N 64000000
+
+long long sigma2[N] = {0};
+
+long long sqrti(long long x) {
+    //integer sqrt function that adjusts sqrtl that is sometimes a little off.
+    //i found this to be much more efficient than doing a pure integer binsrch sqrt.
+    if (x <= 0) return 0;
+    long long r = sqrtl((long double) x);
+    while (r * r > x) r--;
+    return r;
+}
+
+int main(void) {
+    //i just loop over all multiples using a usual sieve method like how i do number of factors in a range.
+    for (long i = 1; i<N; i++) {
+        for (long j = i; j<N; j+=i) {
+            sigma2[j] += i*i;
+        }
+    }
+    long long ans = 0;
+    for (long i = 1; i<N; i++) {
+        long long s = sqrti(sigma2[i]);
+        if (s*s == sigma2[i]) ans += i;
+    }
+    printf("%lld\n", ans);
+    return 0;
+}
